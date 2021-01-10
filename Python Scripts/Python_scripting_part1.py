358713c92e69366fdb12c454e7a3f98f983a03ef
virobot <31761892+virobot@users.noreply.github.com>
2021-01-09 19:25:06 -0800

    Update Python Script Files

---
+++ Python Scripts/Python_scripting_part1.py
@@ -0,0 +1,17 @@
+from array import *
+x=2
+a=0
+apple=["1f","2.95f","8.7f","25.7f","75.8f","223.6f","659.7f","1946.6f","5743.49f","16946.22f","50p"]
+
+for j in range(1,6):
+    z=0
+    a=a+1
+    for i in range(0,11):
+        b=a*64
+        print('.alter case %d:' %x)       
+        print('.param numfin = %d' %j)
+        print('.param LoadCap=',apple[z])
+        x=x+1
+        z=z+1
+        print(" ")
+
